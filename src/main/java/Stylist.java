eb36b12d0c15f67adfb96f7f44b1201a5e77fb05
Matt Rosanio <mmrosani@syr.edu>
2016-02-26 09:18:01 -0800

    Add spec and functionality for all method in Stylist class

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -2,6 +2,26 @@ import org.sql2o.*;
 import java.util.*;
 
 public class Stylist {
+  private int id;
+  private String name;
 
-  
+  public Stylist(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public static List<Stylist> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylists";
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
+
 }
